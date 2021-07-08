ef0eb34ab98e79f8e91660c5d71e06c45f982ca1
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-07-08 11:13:52 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello!')
+let myLibrary = [];
+function Book(name, author, ISBN) {
+  // constructor
+  this.name = name;
+  this.author = author;
+  this.isbn = ISBN;
+}
+function addBookToLibrary(name, author, ISBN) {
+  //create the Book using constructor
+  let newBook = new Book(name, author, ISBN);
+  //add the book to the array
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary('Atomic Habits', 'James Clear', 'ISBN1234');
+addBookToLibrary('Sapiens', 'Yuval Noah Harari', 'ISBN6871');
+addBookToLibrary('The Art of War', 'Sun Tzu', 'ISBN9912');
+addBookToLibrary('the War of Art', 'Steven Pressfield', 'ISBN2191');
+console.log(myLibrary);
