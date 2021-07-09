989539261d07b8bf121e3682697c9ff25a057c31
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-07-09 11:58:02 +0530

    1 file modified

--- script.js
+++ script.js
@@ -17,38 +17,42 @@ addBookToLibrary('Sapiens', 'Yuval Noah Harari', 'ISBN6871');
 addBookToLibrary('The Art of War', 'Sun Tzu', 'ISBN9912');
 addBookToLibrary('The War of Art', 'Steven Pressfield', 'ISBN2191');
 console.log(myLibrary);
-buildLibraryUI();
-function buildLibraryUI() {
-  myLibrary.forEach(element => {
-    console.log(element.name);
-    let bookUI = document.getElementById('BookUI');
-    let bookDiv = document.createElement('div');
-    bookDiv.setAttribute('class', 'Book');
-    let paraName = document.createElement('p');
-    paraName.innerHTML = element.name;
-    paraName.setAttribute('class', 'BookName');
-    let paraAuth = document.createElement('p');
-    paraAuth.innerHTML = element.author;
-    paraAuth.setAttribute('class', 'Author');
-    let paraISBN = document.createElement('p');
-    paraISBN.innerHTML = element.isbn;
-    paraISBN.setAttribute('class', 'ISBN');
-    bookDiv.appendChild(paraName);
-    bookDiv.appendChild(paraAuth);
-    bookDiv.appendChild(paraISBN);
-    bookUI.appendChild(bookDiv);
-  });
-}
-
 let newBtn = document.getElementById('newBookBtn');
 newBtn.onclick = function() {
   let overlay = document.getElementById('overlay');
   overlay.style.display = 'block';
 };
 function OnSubmit() {
-  let bookName = document.getElementById('newBookName').textContent;
-  let bookAuth = document.getElementById('newBookAuth').textContent;
-  let bookISBN = document.getElementById('newBookISBN').textContent;
+  let bookName = document.getElementById('newBookName').value;
+  let bookAuth = document.getElementById('newBookAuthor').value;
+  let bookISBN = document.getElementById('newBookISBN').value;
   addBookToLibrary(bookName, bookAuth, bookISBN);
   document.getElementById('overlay').style.display = 'none';
+  console.log(myLibrary);
+  addCard(bookName, bookAuth, bookISBN);
+}
+buildLibraryUI();
+function buildLibraryUI() {
+  myLibrary.forEach(element => {
+    console.log(element.name);
+    addCard(element.name, element.author, element.isbn);
+  });
+}
+function addCard(bookName, bookAuth, bookISBN) {
+  let bookUI = document.getElementById('BookUI');
+  let bookDiv = document.createElement('div');
+  bookDiv.setAttribute('class', 'Book');
+  let paraName = document.createElement('p');
+  paraName.innerHTML = bookName;
+  paraName.setAttribute('class', 'BookName');
+  let paraAuth = document.createElement('p');
+  paraAuth.innerHTML = bookAuth;
+  paraAuth.setAttribute('class', 'Author');
+  let paraISBN = document.createElement('p');
+  paraISBN.innerHTML = bookISBN;
+  paraISBN.setAttribute('class', 'ISBN');
+  bookDiv.appendChild(paraName);
+  bookDiv.appendChild(paraAuth);
+  bookDiv.appendChild(paraISBN);
+  bookUI.appendChild(bookDiv);
 }
